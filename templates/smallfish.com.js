d857913a245f4d0a0ce517987a0f6a4f91710168
rporrinis <riccardo@smallfish.com>
2016-10-25 15:43:21 +0200

    www

--- templates/smallfish.com.js
+++ templates/smallfish.com.js
@@ -1,12 +1,13 @@
-module.exports = env => (`
-  server {
+module.exports = env => {
+  const prefix = env === '' ? 'www.' : env;
+  return (`server {
           listen 80;
-          server_name ${env}smallfish.com;
-          server_name ${env}sml-server.com;
+          server_name ${prefix}smallfish.com;
+          server_name ${prefix}sml-server.com;
 
           location / {
             proxy_pass		http://${env}services.sml-server.com:80/;
       			proxy_redirect		off;
           }
-  }
-`);
+    }`)
+};
