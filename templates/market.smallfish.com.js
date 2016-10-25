bc482ebf636044ce2452266ce5dfad74fc6cfce8
rporrinis <riccardo@smallfish.com>
2016-10-25 10:57:33 +0200

    reverted the proxy for websockets

--- templates/market.smallfish.com.js
+++ templates/market.smallfish.com.js
@@ -18,13 +18,5 @@ module.exports = env => (`
               proxy_pass		http://${env}upload.smallfish.com/;
         			proxy_redirect		off;
             }
-
-            location /socket.io {
-                proxy_pass http://${env}live.smallfish.com;
-                proxy_http_version 1.1;
-                proxy_set_header Upgrade $http_upgrade;
-                proxy_set_header Connection "upgrade";
-                proxy_set_header Host $host;
-            }
     }
 `);
