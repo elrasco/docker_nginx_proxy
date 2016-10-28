e0b9eb4b31b88e16a612378cbe6b5b302a2d6df1
Luca Rasconi <luca.rasconi@gmail.com>
2016-10-28 14:33:19 +0200

    upl.smallfish.com

--- templates/upload.smallfish.com.js
+++ templates/upload.smallfish.com.js
@@ -1,18 +1,11 @@
 module.exports = env => (`
   server {
     listen 80;
+    server_name ${env}upl.smallfish.com;
     server_name ${env}upload.smallfish.com;
     server_name ${env}upload.sml-server.com;
 
     location / {
-      if ($request_method = OPTIONS ) {
-        add_header Access-Control-Allow-Origin "*";
-        add_header Access-Control-Allow-Methods "GET, OPTIONS";
-        add_header Access-Control-Allow-Headers "Authorization";
-        add_header Access-Control-Allow-Credentials "true";
-        add_header Content-Length 0;
-        add_header Content-Type text/plain;
-      }
       proxy_pass		http://${env}services.sml-server.com:82/;
       proxy_redirect		off;
     }
