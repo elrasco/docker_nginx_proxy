7b2a3ecbec69de8de08215d5939f21f1cf299ad7
Luca Rasconi <luca.rasconi@gmail.com>
2016-10-28 14:21:35 +0200

    options proxypass

--- templates/upload.smallfish.com.js
+++ templates/upload.smallfish.com.js
@@ -5,6 +5,14 @@ module.exports = env => (`
     server_name ${env}upload.sml-server.com;
 
     location / {
+      if ($request_method = OPTIONS ) {
+        add_header Access-Control-Allow-Origin "*";
+        add_header Access-Control-Allow-Methods "GET, OPTIONS";
+        add_header Access-Control-Allow-Headers "Authorization";
+        add_header Access-Control-Allow-Credentials "true";
+        add_header Content-Length 0;
+        add_header Content-Type text/plain;
+      }
       proxy_pass		http://${env}services.sml-server.com:82/;
       proxy_redirect		off;
     }
