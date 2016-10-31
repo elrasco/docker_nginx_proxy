47d653b3dd874341301a7845f107fe362b5e9c28
rporrinis <riccardo@smallfish.com>
2016-10-31 16:10:24 +0100

    first tests to ensure retrocompatibility

--- test/test.suite.js
+++ test/test.suite.js
@@ -1,14 +1,57 @@
 const request = require('supertest-promised');
 
-describe('status configuration', function() {
+const on = data => request('http://localhost')
+  .get(data.url)
+  .set('Host', data.host)
+  .expect(data.expect);
 
-  it('should be available to requests in default host', function() {
+describe('alive page', function() {
 
-    return request('http://localhost')
-      .get('/status/alive')
-      .set('Host', 'any.host')
-      .expect(200);
+  it('should be available to requests to default host', function() {
+
+    return on({
+      url: '/status/alive',
+      host: 'any.host',
+      expect: 200
+    });
+
+  });
+});
+
+describe('smallfish.com', function() {
+
+  it('should be available to requests to smallfish.com', function() {
+
+    return on({
+      url: '/',
+      host: 'dev.smallfish.com',
+      expect: /<title>Smallfish<\/title>/
+    });
 
   });
 
+});
+
+
+describe('market.smallfish.com', function() {
+
+  it('should be available to requests to market.smallfish.com', function() {
+
+    return on({
+      url: '/',
+      host: 'dev.market.smallfish.com',
+      expect: /<title>Smallfish - Marketplace<\/title>/
+    });
+
+  });
+
+  it('should be reverse requests to backend', function() {
+
+    return on({
+      url: '/marketplace-be/auth/authenticated',
+      host: 'dev.market.smallfish.com',
+      expect: {"auth": false, "guest": false}
+    });
+
+  });
 });
