f943092b31fe8a5cc51c6f36ca5ad36ec8dea33e
rporrinis <riccardo@smallfish.com>
2016-12-05 15:13:37 +0100

    tested url rewriting

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,4 @@ host_ip=$(ifconfig docker0 | grep inet | grep -v inet6 | cut -d' ' -f12 | cut -d
 
 node index.js --env=$1
 docker build -t smallfish_proxy .
-docker run -p 80:80 --add-host dev.services.sml-server.com:$host_ip -d --name smallfish_proxy smallfish_proxy:latest
+docker run -p 80:80 --add-host dev.services.internal.sml-server.com:$host_ip -d --name smallfish_proxy smallfish_proxy:latest
--- test/test.suite.js
+++ test/test.suite.js
@@ -13,3 +13,44 @@ describe('alive page', function() {
 
   });
 });
+
+describe('admin.smallfish.com proxy', function() {
+
+  describe('to marketplace-api', function() {
+    let server;
+    beforeEach(done => {
+      server = echo(1341);
+      done();
+    });
+    afterEach(done => {
+      server.close();
+      done();
+    });
+    it('should rewrite urls', function() {
+      return on({
+        url: '/marketplace-api/api/rest',
+        host: 'dev.admin.smallfish.com',
+        expect: /"uri":"\/\/api\/rest"/
+      });
+    });
+  });
+
+  describe('to smallfish-auth', function() {
+    let server;
+    beforeEach(done => {
+      server = echo(1343);
+      done();
+    });
+    afterEach(done => {
+      server.close();
+      done();
+    });
+    it('should rewrite urls', function() {
+      return on({
+        url: '/smallfish-auth/api/rest',
+        host: 'dev.admin.smallfish.com',
+        expect: /"uri":"\/\/api\/rest"/
+      });
+    });
+  });
+});
