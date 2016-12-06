eeecb2e1e375b957a8c35f2c91da3da631e253a9
rporrinis <riccardo@smallfish.com>
2016-12-06 10:28:31 +0100

    fix the mess

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,12 +2,16 @@
 
 host_ip=$(ifconfig docker0 | grep inet | grep -v inet6 | cut -d' ' -f12 | cut -d':' -f2)
 
+if [ $2 == --testing ]
+then
+  host_mappings="--add-host dev.services.internal.sml-server.com:${host_ip} --add-host dev.services.sml-server.com:${host_ip}"
+fi
+
 node index.js --env=$1
 docker build -t smallfish_proxy .
 docker run \
 -p 80:80 \
---add-host dev.services.internal.sml-server.com:$host_ip \
---add-host dev.services.sml-server.com:$host_ip \
+$host_mappings \
 -d \
 --name smallfish_proxy \
 smallfish_proxy:latest
