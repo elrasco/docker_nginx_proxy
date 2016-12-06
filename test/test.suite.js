77ddf9438c1efc78b0a114ad0575db2a3dcee28f
rporrinis <riccardo@smallfish.com>
2016-12-06 16:13:27 +0100

    test

--- test/test.suite.js
+++ test/test.suite.js
@@ -1,56 +1,13 @@
-const echo = require('./echo');
 const on = require('./testRequest');
 
 describe('alive page', function() {
 
   it('should be available to requests to default host', function() {
-
     return on({
       url: '/status/alive',
       host: 'any.host',
       expect: 200
     });
-
   });
-});
-
-describe('admin.smallfish.com proxy', function() {
-
-  describe('to marketplace-api', function() {
-    let server;
-    beforeEach(done => {
-      server = echo(1341);
-      done();
-    });
-    afterEach(done => {
-      server.close();
-      done();
-    });
-    it('should rewrite urls', function() {
-      return on({
-        url: '/marketplace-api/api/rest',
-        host: 'dev.admin.smallfish.com',
-        expect: /"uri":"\/\/api\/rest"/
-      });
-    });
-  });
-
-  describe('to smallfish-auth', function() {
-    let server;
-    beforeEach(done => {
-      server = echo(1343);
-      done();
-    });
-    afterEach(done => {
-      server.close();
-      done();
-    });
-    it('should rewrite urls', function() {
-      return on({
-        url: '/smallfish-auth/api/rest',
-        host: 'dev.admin.smallfish.com',
-        expect: /"uri":"\/\/api\/rest"/
-      });
-    });
-  });
+  
 });
--- test/testRequest.js
+++ test/testRequest.js
@@ -1,8 +1,14 @@
 const request = require('supertest-promised');
 
+const urls = {
+  test: 'http://localhost',
+  development: 'http://dev.proxy.sml-server.com',
+  stage: 'http://stage.proxy.sml-server.com',
+  production: 'http://proxy.sml-server.com'
+};
+
 module.exports = data => {
-  const port = data.port ? data.port : 80;
-  return request('http://localhost:' + port)
+  return request(urls[process.env.NODE_ENV])
             .get(data.url)
             .set('Host', data.host)
             .expect(data.expect);
