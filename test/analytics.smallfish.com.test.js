e0cd16b106ef2e0b8dce312025a6b60ee8b9e9be
rporrinis <riccardo@smallfish.com>
2016-12-13 12:07:20 +0100

    included the analytics hosts

---
+++ test/analytics.smallfish.com.test.js
@@ -0,0 +1,15 @@
+const request = require('./testRequest');
+
+//test('analytics.smallfish.com');
+//test('analytics.sml-server.com');
+
+function test(host) {
+  describe(host, function() {
+    it('/', function() {
+      return request.options({
+        url: '/',
+        host: host
+      });
+    });
+  });
+}
