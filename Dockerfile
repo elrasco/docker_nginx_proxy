541f95cbb2ea812a3cf5a45a05afa9aec9658f32
rporrinis <riccardo@smallfish.com>
2016-12-21 10:25:14 +0100

    now we are able to execute all the tests in local

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM nginx
 
+# Convenience tools
+RUN apt-get update && \
+    apt-get install -y iptables curl
+
 # Main configs
 RUN rm /etc/nginx/nginx.conf
 COPY nginx.conf /etc/nginx
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,5 @@
 
 scripts/build.sh $1
 docker build -t smallfish_proxy .
-docker run -p 80:80 -d --name smallfish_proxy smallfish_proxy:latest
+docker run -p 80:80 -d --privileged --name smallfish_proxy smallfish_proxy:latest
+docker exec -it smallfish_proxy iptables -t nat -A OUTPUT -d 172.31.0.2 -j DNAT --to-destination 8.8.8.8
