42cdb2c0f51b3ed46dd442679bb7248e81f231fa
Shy Alter <puemos@gmail.com>
2017-04-07 10:52:56 +0200

    fuguplay /home/

--- templates/fuguplay.com.js
+++ templates/fuguplay.com.js
@@ -24,6 +24,13 @@ module.exports = (env, raw_env) => {
             proxy_pass  $backend;
             proxy_redirect		off;
           }
+          location = /home/ {
+            set $backend "http://website-fuguplay-homepage-${raw_env}.s3-website-eu-west-1.amazonaws.com";
+            rewrite /(?!.*js|.*ico|.*css) / break;
+            rewrite ^/(.*)/$ $1 break;
+            proxy_pass  $backend;
+            proxy_redirect		off;
+          }
 
           location = /profilo/campaigns/ {
             set $backend "http://website-fuguplay-fe-${raw_env}.s3-website-eu-west-1.amazonaws.com";
