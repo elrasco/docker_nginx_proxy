1410086f45ab81b6cc620785de08966c3d3ca336
rporrinis <riccardo@smallfish.com>
2017-05-09 16:13:59 +0200

    changed smallfish mobile route

--- templates/m.smallfish.com.js
+++ templates/m.smallfish.com.js
@@ -1,11 +1,11 @@
-module.exports = env => (`
+module.exports = (env, raw_env) => (`
   server {
           listen 80;
           server_name ${env}m.smallfish.com;
           server_name ${env}m.sml-server.com;
 
           location / {
-            set $backend "http://${env}services.sml-server.com:93";
+            set $backend "http://website-smallfish-homepage-${raw_env}.s3-website-eu-west-1.amazonaws.com";
             proxy_pass  $backend;
             proxy_redirect		off;
           }
