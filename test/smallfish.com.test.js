7d3a4d74ed8d8c78f937f878a0fcf51837552cc0
rporrinis <riccardo@smallfish.com>
2017-05-12 14:36:10 +0200

    now the are no more headers from amazon

--- test/smallfish.com.test.js
+++ test/smallfish.com.test.js
@@ -14,5 +14,17 @@ function test(host) {
       });
     });
 
+    it('/ routes headers should be obfuscated', function() {
+      return request.frontend({
+          url: '/',
+          host
+        })
+        .expect(response => {
+          const areThereAnyAmazonHeaders = Object.keys(response.headers).filter(header => header.indexOf('x-amz') > -1);
+          if (areThereAnyAmazonHeaders.length > 0) {
+            throw new Error(`Expected no amazon headers in the response. Got [${areThereAnyAmazonHeaders}]`);
+          }
+        });
+    });
   });
 }
